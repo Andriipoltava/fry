2827814aa35bd30a3dc2c808a11c585279e7fe28
devdomcode <dev@domcode.co>
2025-02-25 17:46:25 +0200

    tedst2SADe

--- blocks/about/about.php
+++ blocks/about/about.php
@@ -41,7 +41,6 @@ if (!empty($block['align'])) {
             <div class="col-12 offset-xxl-1 col-xl-11  py-sm-5 my-lg-5 pb-3 mb-md-5 px-4">
                 <?php if ($subtitle) echo "<span class='uppercase-subtitles'> $subtitle</span>"; ?>
                 <?php if ($title) echo "<h2> $title</h2>"; ?>
-
             </div>
             <div class="d-md-none" style="    height: 200px;"></div>
 
--- blocks/only-title/only-title.php
+++ blocks/only-title/only-title.php
@@ -27,13 +27,18 @@ if (!empty($block['align'])) {
     $class_name .= ' align' . $block['align'];
 }
 
-
+$ar_pc = get_field('image_proportions_to_pc') ? get_field('image_proportions_to_pc') . '-xl' : 'arv-52-xl';
+$ar_laptop = get_field('image_proportions_to_laptop') ? get_field('image_proportions_to_laptop') . '-lg' : 'arv-52-lg';
+$ar_tablet = get_field('image_proportions_to_tablet') ? get_field('image_proportions_to_tablet') . '-md' : 'arv-54-md ';
+$ar_mobile = get_field('image_proportions_to_mobile') ? get_field('image_proportions_to_mobile') : ' arv-54';
 ?>
 
-<div <?php echo esc_attr($anchor); ?>class="<?php echo esc_attr($class_name); ?> bg-dark ">
-    <div class="container-fluid  py-5 text-white"    style="background-image: url(<?php echo $image ? wp_get_attachment_image_url($image['ID'], 'full') : '' ?>);background-size: cover;background-repeat: no-repeat;background-position: center;background-color:#252525 ">
-        <div class="row   d-flex align-items-lg-end  justify-content-center text-center">
-            <div class="col-lg-10 col-xxl-6   py-5 my-lg-5 mb-5">
+<div <?php echo esc_attr($anchor); ?>class="<?php echo esc_attr($class_name); ?> bg-dark    ">
+    <div class="overflow-hidden d-flex align-items-center  py-2 text-white   <?php echo $ar_pc . ' ' . $ar_laptop . ' ' . $ar_tablet . ' ' . $ar_mobile ?> "
+         style="background-image: url(<?php echo $image ? wp_get_attachment_image_url($image['ID'], 'full') : '' ?>);background-size: cover;background-repeat: no-repeat;background-position: center;background-color:#252525 ">
+        <div class="container">
+        <div class="row   d-flex align-items-center  justify-content-center text-center">
+            <div class="col-lg-10 col-xxl-8   px-xxl-4">
                 <?php if ($subtitle) echo "<span class='uppercase-subtitles'> $subtitle</span>"; ?>
                 <?php if ($title) echo "<h1 class='h2 lh-1' > $title</h1>"; ?>
 
